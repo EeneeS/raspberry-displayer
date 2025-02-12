a33ea3eb9ae5efbf837418edec2089278f801387
Niels Soete <soeteniels1803@gmail.com>
2025-02-12 17:25:14 +0100

    multer storage variable

--- server/server.js
+++ server/server.js
@@ -2,12 +2,22 @@ import express from "express";
 import { fileURLToPath } from "url";
 import path from "path";
 import fs from "fs";
+import multer from "multer";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
 
 const app = express();
 const port = 1234;
+
+const storage = multer.diskStorage({
+  destination: function(_req, _res, cb) {
+    cb(null, path.join(__dirname, "../uploads/"));
+  },
+  filename: function(_req, _res, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
 
 app.use(express.json());
 
