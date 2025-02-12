fdfb7083f6e661b6cc464f5311402631b6c7d187
Niels Soete <soeteniels1803@gmail.com>
2025-02-12 17:04:21 +0100

    endpoint to retrieve the file names, serve the uploads folder.

---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
+const app = express();
+const port = 1234;
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Raspberry Displayer");
+});
+
+app.use(express.static(path.join(__dirname, "../uploads")));
+app.get("/images", (_req, res) => {
+  const uploadDir = path.join(__dirname, "../uploads/");
+  try {
+    const files = fs.readdirSync(uploadDir)
+    res.send(files);
+  } catch (error) {
+    console.log(error);
+  };
+});
+
+app.listen(port, () => {
+  console.log(`[server] running on port: ${port}`);
+});
