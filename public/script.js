1fabb75f4c09429c5dc7dd37b32babc60a342d3b
Niels Soete <soeteniels1803@gmail.com>
2025-02-12 17:56:11 +0100

    request the image names from the server, display them on the client

--- public/script.js
+++ public/script.js
@@ -1,1 +1,28 @@
 "use strict";
+
+async function loadImages() {
+  const $imageContainer = document.querySelector("#images");
+  const imageNames = await getImageNames();
+  const html = createImageHTML(imageNames);
+  $imageContainer.insertAdjacentHTML("beforeend", html);
+};
+
+async function getImageNames() {
+  const response = await fetch("http://localhost:1234/images");
+  const data = await response.json();
+  return data;
+};
+
+function createImageHTML(imageNames) {
+  let html = "";
+  imageNames.forEach((name) => {
+    html += `<li><img src="http://localhost:1234/uploads/${name}"></li>`;
+  });
+  return html;
+}
+
+function main() {
+  loadImages();
+};
+
+main();
