fec6b92eae560b8f434bee213d598ece9f77e734
Niels Soete <soeteniels1803@gmail.com>
2025-02-17 11:17:03 +0100

    add cors

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { fileURLToPath } from "url";
 import path from "path";
 import fs from "fs";
@@ -9,6 +10,8 @@ const __dirname = path.dirname(__filename)
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "../public")));
 
